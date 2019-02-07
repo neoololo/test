05f71a310206a4cffbd4d7a6b98146e10dcdc06e
neo <neoololoo@gmail.com>
2019-02-07 17:28:46 +0300

    the second commit

--- project 2/script.js
+++ project 2/script.js
@@ -5,26 +5,12 @@ let money, time, expenses = {};
 money = prompt('Ваш бюджет на месяц?', '');
 time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-// for (let i = 0; i < 2; i++) {
-//    let expenditure = prompt('Введите обязательную статью расходов в этом месяце?', '');
-//    let money = prompt('Во сколько обойдется?', '');
-//    expenses[expenditure] = money;
-// }
+for (let i = 0; i < 2; i++) {
+   let expenditure = prompt('Введите обязательную статью расходов в этом месяце?', '');
+   let money = prompt('Во сколько обойдется?', '');
+   expenses[expenditure] = money;
+}
 
-let i = 0;
-// while(i < 2) {
-//     let expenditure = prompt('Введите обязательную статью расходов в этом месяце?', '');
-//     let money = prompt('Во сколько обойдется? ' + expenditure, '');
-//     expenses[expenditure] = money;
-//     i++;
-// }
-
-do {
-    let expenditure = prompt('Введите обязательную статью расходов в этом месяце?', '');
-    let money = prompt('Во сколько обойдется? ' + expenditure, '');
-    expenses[expenditure] = money;
-    i++;
-} while (i < 2)
 
 let appData = {
     budget: money,
